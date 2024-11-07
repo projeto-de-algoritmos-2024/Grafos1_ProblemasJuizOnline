f5f9e47bd75197fee3bdbbec05c44d9279277f6f
Cecilia <ceciquaresma2014@gmail.com>
2024-11-07 18:15:27 -0300

    codigo final dfs

--- ExisteCaminho/DFS.cpp
+++ ExisteCaminho/DFS.cpp
@@ -3,13 +3,14 @@
 #include <map>
 #include <vector>
 #include <queue>
+#include <stack>
 using namespace std;
 
-class Grafo { 
+class Solution { 
 
 public:
   
-    bool temCaminho(int numNos, vector<vector<int>> arestas, int inicio, int final){
+    bool validPath(int numNos, vector<vector<int>> arestas, int inicio, int final){
             if(inicio == final) return true;
             
             vector<vector<int>> listaAdj(numNos);
@@ -18,23 +19,26 @@ public:
                 listaAdj[v[0]].push_back(v[1]);
                 listaAdj[v[1]].push_back(v[0]);
             }
-            map<int, bool> visitado;
+            vector<bool> visitado(numNos, false);
             return DFS(inicio, final, listaAdj, visitado);
 }
 
-    bool DFS(int u, int final, vector<vector<int>>&listaAdj, map<int, bool> &visitado) {
-        if (u == final) {
-            return true; 
-        }
+    bool DFS(int inicio, int final, vector<vector<int>>&listaAdj, vector<bool> &visitado) {
+        stack<int> pilha;
+        pilha.push(inicio);
+        visitado[inicio] = true;
 
-        visitado[u] = true;
-
-        for (auto v : listaAdj[u]) {
-            if (!visitado[v]) {
-                if (DFS(v, final, listaAdj, visitado)) {
-                    return true; 
+        while(!pilha.empty()){
+            int u = pilha.top();
+            pilha.pop();
+            if (u == final) return true; 
+        
+            for (auto v : listaAdj[u]) {
+                if (!visitado[v]) {
+                    visitado[v] = true;
+                    pilha.push(v);
+                    }
                 }
-            }
         }
         
         return false; 
